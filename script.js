248af37c0af9dd0e683f2a53bc3174ef8f283e6f
volkantemelli <volkan.temelli@gmail.com>
2025-04-03 18:50:21 +0200

    refactor game layout and add dice functionality

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 "use Strict";
-/* 
-    <h1 class="game-title">Risky Pig</h1>
-    <button class="start-button">NEW GAME</button>
-    <h3 class="player-name">PLAYER 1</h3>
-    <p class="first-player-total-score">Total score: X</p>
-    <p class="first-player-current-score">Current score: X</p>
-    <h3 class="player-name">PLAYER 2</h3>
-    <p class="second-player-total-score">Total score: X</p>
-    <p class="second-player-total-score">Current score: X</p>
-    <button class="dice-button">ROLL</button>
-    <button class="hold-button">HOLD</button>
-*/
 
 // Select elements
 const startButton = document.querySelector(".start-button");
@@ -28,3 +16,36 @@ const secondPlayerCurrentScore = document.querySelector(
 );
 const diceButton = document.querySelector(".dice-button");
 const holdButton = document.querySelector(".hold-button");
+const diceResultSpan = document.querySelector(".dice-result");
+
+// Declare constant for dice roll
+let currentDice;
+
+// Define functions
+
+// Reset game
+const resetGame = () => {
+  // Set current dice to 0
+  currentDice = 0;
+  // Display dice result as 0
+  displayText(diceResultSpan, currentDice);
+};
+
+// Roll dice to start the game
+const rollDice = () => {
+  // Assign a random number between 1 - 6 to currentDice
+  currentDice = Math.ceil(Math.random() * 6);
+  console.log("current dice: ", currentDice);
+  // Display current dice result
+  displayText(diceResultSpan, currentDice);
+};
+
+//Display text content of an element
+const displayText = (element, content) => {
+  element.textContent = content;
+};
+
+// Add event listener to diceButton for dice rolling
+diceButton.addEventListener("click", rollDice);
+// Add event listener to new game button for resetting dice and scores
+startButton.addEventListener("click", resetGame);

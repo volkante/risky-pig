0e0cb7836d7c582faaf03021242f221af542d5f9
volkantemelli <volkan.temelli@gmail.com>
2025-04-04 21:37:30 +0200

    refactor player score display and enhance turn management functionality

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 "use Strict";
 
 // Select html elements and assign them to variables
+const firstPlayerSection = document.querySelector(".first-player-container");
+const secondPlayerSection = document.querySelector(".second-player-container");
 const startButton = document.querySelector(".start-button");
 const firstPlayerTotalScoreSpan = document.querySelector(
-  ".first-player-total-score"
+  ".first-player-total-score span"
 );
 const firstPlayerCurrentScoreSpan = document.querySelector(
-  ".first-player-current-score"
+  ".first-player-current-score span"
 );
 const secondPlayerTotalScoreSpan = document.querySelector(
-  ".second-player-total-score"
+  ".second-player-total-score span"
 );
+
 const secondPlayerCurrentScoreSpan = document.querySelector(
-  ".second-player-current-score"
+  ".second-player-current-score span"
 );
 const diceButton = document.querySelector(".dice-button");
 const holdButton = document.querySelector(".hold-button");
@@ -30,10 +33,17 @@ let secondPlayerTotalScore;
 // Reset game
 const resetGame = () => {
   if (currentDice) {
-    // Set current dice to null
-    currentDice = null;
-    // Display an empty string in the place of dice
+    resetTurn();
+    currentDice = 0;
+    firstPlayerCurrentScore = 0;
+    secondPlayerCurrentScore = 0;
+    firstPlayerTotalScore = 0;
+    secondPlayerTotalScore = 0;
     displayText(diceResultSpan, currentDice);
+    displayText(firstPlayerCurrentScoreSpan, firstPlayerCurrentScore);
+    displayText(firstPlayerTotalScoreSpan, firstPlayerTotalScore);
+    displayText(secondPlayerCurrentScoreSpan, secondPlayerCurrentScore);
+    displayText(secondPlayerTotalScoreSpan, secondPlayerTotalScore);
   }
 };
 
@@ -41,14 +51,43 @@ const resetGame = () => {
 const rollDice = () => {
   // Assign a random number between 1 - 6 to currentDice
   currentDice = Math.ceil(Math.random() * 6);
+  // Set current dice to firstPlayerCurrentScore
+  firstPlayerCurrentScore = currentDice;
+  if (currentDice == 1) {
+    changeTurn();
+  }
+
   console.log("current dice: ", currentDice);
   // Display current dice result
   displayText(diceResultSpan, currentDice);
+  displayText(firstPlayerCurrentScoreSpan, firstPlayerCurrentScore);
+  displayText(firstPlayerTotalScoreSpan, firstPlayerTotalScore);
+  displayText(secondPlayerCurrentScoreSpan, secondPlayerCurrentScore);
+  displayText(secondPlayerTotalScoreSpan, secondPlayerTotalScore);
 };
 
 // Display text content of an element
 const displayText = (element, content) => {
   element.textContent = content;
+};
+
+// Change turn by adding or removing "playing" class
+const changeTurn = () => {
+  if (firstPlayerSection.classList.contains("playing")) {
+    firstPlayerSection.classList.remove("playing");
+    secondPlayerSection.classList.add("playing");
+  } else {
+    secondPlayerSection.classList.remove("playing");
+    firstPlayerSection.classList.add("playing");
+  }
+};
+
+// Reset turn by making player 1 playing
+const resetTurn = () => {
+  if (secondPlayerSection.classList.contains("playing")) {
+    secondPlayerSection.classList.remove("playing");
+    firstPlayerSection.classList.add("playing");
+  }
 };
 
 ////////// Add event listeners /////////////
